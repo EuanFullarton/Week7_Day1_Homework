c19766759886c18e63752787d9d8a9f200e8bb03
Euan Fullarton <euanfullarton7@gmail.com>
2017-04-17 15:28:24 +0100

    created Movie class

---
+++ app/src/main/java/example/codeclan/com/week7_day1_homework/Movie.java
@@ -0,0 +1,14 @@
+package example.codeclan.com.week7_day1_homework;
+
+/**
+ * Created by user on 17/04/2017.
+ */
+
+public class Movie {
+
+    private String title;
+    private String genre;
+    private int ranking;
+
+    
+}
