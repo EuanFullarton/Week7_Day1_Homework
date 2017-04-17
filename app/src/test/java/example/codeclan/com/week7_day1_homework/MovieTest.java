9a7e0a4d8f6328d344c894f603b353ff31ad2256
Euan Fullarton <euanfullarton7@gmail.com>
2017-04-17 15:51:00 +0100

    testPrintMovieDeatils created

--- app/src/test/java/example/codeclan/com/week7_day1_homework/MovieTest.java
+++ app/src/test/java/example/codeclan/com/week7_day1_homework/MovieTest.java
@@ -1,5 +1,6 @@
 package example.codeclan.com.week7_day1_homework;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -10,33 +11,42 @@ import static junit.framework.Assert.assertEquals;
 
 public class MovieTest {
 
+    Movie movie;
+
+    @Before
+    public void before() {
+        movie = new Movie("Die Hard", "Action", 1);
+    }
+
+
     @Test
-    public void testMovieSetup(){
-        Movie movie = new Movie("Die Hard", "Action", 1);
+    public void testMovieSetup() {
         assertEquals("Die Hard", movie.getTitle());
         assertEquals("Action", movie.getGenre());
         assertEquals(1, movie.getRanking());
     }
 
     @Test
-    public void testSetTitle(){
-        Movie movie = new Movie("Die Hard", "Action", 1);
+    public void testSetTitle() {
         movie.setTitle("Lethal Weapon");
         assertEquals("Lethal Weapon", movie.getTitle());
     }
 
     @Test
-    public void testSetGenre(){
-        Movie movie = new Movie("Die Hard", "Action", 1);
+    public void testSetGenre() {
         movie.setGenre("Comedy");
         assertEquals("Comedy", movie.getGenre());
     }
 
     @Test
-    public void testSetRanking(){
-        Movie movie = new Movie("Die Hard", "Action", 1);
+    public void testSetRanking() {
         movie.setRanking(5);
         assertEquals(5, movie.getRanking());
+    }
+
+    @Test
+    public void testPrintMovieDetails() {
+        assertEquals("Title: 'Die Hard', Genre: 'Action', Ranking: 1", movie.printMovieDetails();)
     }
 
 }
